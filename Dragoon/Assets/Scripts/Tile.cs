6919578190ced2705e72ea211ce7472b17ff4664
jgy2 <jgy2@njit.edu>
2022-09-26 20:41:06 -0400

    ranges fixed for impassable terain

---
+++ Dragoon/Assets/Scripts/Tile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject attackSquare;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Impassable"))
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Mountain") && this.gameObject.CompareTag("Move"))
+        {
+            Instantiate(attackSquare, transform.position, new Quaternion(0, 0, 0, 0), transform.parent.transform);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Dragoon/Assets/Scripts/Unit.cs
+++ Dragoon/Assets/Scripts/Unit.cs
@@ -27,6 +27,7 @@ namespace unit
         public void getMoveVision()
         {
             Vector3 tempOffset = new Vector3(0, 0, 0);
+            int nonsense = 0;
             for (int bogus = 0; bogus <= mov; bogus ++)
             {
                 tempOffset = new Vector3(squareOffsetX, squareOffsetY + bogus, 0.01f);
@@ -39,11 +40,13 @@ namespace unit
                 }
                 else
                 {
+                    tempOffset = new Vector3(squareOffsetX, squareOffsetY + bogus, -0.01f);
                     Instantiate(attackSquare, (transform.position - (tempOffset - new Vector3(0, 1, 0))) + (new Vector3(1, 0, 0) * (bogus - (mov + 1))), new Quaternion(0, 0, 0, 0), transform);
                 }
 
                 for (int bogus2 = (mov * -1) + bogus; bogus2 <= mov - bogus; bogus2++)
                 {
+                    tempOffset = new Vector3(squareOffsetX, squareOffsetY + bogus, 0.01f);
                     Instantiate(moveSquare, (transform.position + tempOffset) + (new Vector3(1, 0, 0) * bogus2), new Quaternion(0, 0, 0, 0), transform);
 
                     if (bogus == 0)
@@ -52,10 +55,12 @@ namespace unit
                     }
                     else
                     {
+                        tempOffset = new Vector3(squareOffsetX, squareOffsetY + bogus, -0.01f);
                         Instantiate(moveSquare, (transform.position - (tempOffset - new Vector3(0, 1, 0))) + (new Vector3(1, 0, 0) * bogus2), new Quaternion(0, 0, 0, 0), transform);
                     }
                 }
 
+                tempOffset = new Vector3(squareOffsetX, squareOffsetY + bogus, 0.01f);
                 Instantiate(attackSquare, (transform.position + tempOffset) + (new Vector3(1, 0, 0) * ((mov + 1) - bogus)), new Quaternion(0, 0, 0, 0), transform);
 
                 if (bogus == 0)
@@ -64,11 +69,16 @@ namespace unit
                 }
                 else
                 {
+                    tempOffset = new Vector3(squareOffsetX, squareOffsetY + bogus, -0.01f);
                     Instantiate(attackSquare, (transform.position - (tempOffset - new Vector3(0, 1, 0))) + (new Vector3(1, 0, 0) * ((mov + 1) - bogus)), new Quaternion(0, 0, 0, 0), transform);
                 }
+
+                nonsense = bogus;
             }
 
+            tempOffset = new Vector3(squareOffsetX, squareOffsetY + nonsense, 0.01f);
             Instantiate(attackSquare, transform.position + (tempOffset + new Vector3(0, 1, 0)), new Quaternion(0, 0, 0, 0), transform);
+            tempOffset = new Vector3(squareOffsetX, squareOffsetY + nonsense, -0.01f);
             Instantiate(attackSquare, transform.position - tempOffset, new Quaternion(0, 0, 0, 0), transform);
             //worry about terrain later
         }
